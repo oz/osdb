95c030104fb89667fbb1620879cdf25a6d8041a2
Arnaud Berthomier <oz@cyprio.net>
2014-03-04 23:22:48 -0600

    Add basic osdb.SearchForFile
     - XML-RPC... :mask:
     - mattn/go-xmlrpc is buggy :cry:

--- osdb.go
+++ osdb.go
@@ -11,12 +11,27 @@ import (
 	"encoding/binary"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/mattn/go-xmlrpc"
 )
 
 const (
-	ChunkSize = 65536 // 64k
+	ChunkSize     = 65536 // 64k
+	Server        = "http://api.opensubtitles.org/xml-rpc"
+	SearchLimit   = 100
+	SuccessStatus = "200 OK"
 )
 
+var (
+	UserAgent = "SubDownloader 2.0.10" // FIXME register a user-agent, one day.
+	Token     = ""
+)
+
+type Result struct {
+}
+
+// Generate a OSDB hash for a file
 func Hash(path string) (hash uint64, err error) {
 	// Check file size.
 	fi, err := os.Stat(path)
@@ -57,7 +72,86 @@ func Hash(path string) (hash uint64, err error) {
 	return hash + uint64(fi.Size()), nil
 }
 
-// Fill buffer
+// Search subtitles in `languages` for a file at `path`.
+func SearchForFile(path string, langs []string) ([]Result, error) {
+	// Login anonymously if no token has been set yet.
+	if Token == "" {
+		tok, err := Login("", "", "")
+		Token = tok
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Get file size
+	fi, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+	size := fi.Size()
+
+	// Get file hash
+	h, err := Hash(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Query OSDB....
+	res, err := xmlrpc.Call(Server,
+		"SearchSubtitles",
+		Token,
+		[1]map[string]interface{}{ // go-xmlrpc panics on slices...
+			map[string]interface{}{
+				"moviehash":     fmt.Sprintf("%x", h),
+				"moviebytesize": size,
+				"sublanguageid": strings.Join(langs, ","),
+			},
+		},
+		map[string]int{"limit": SearchLimit},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return parseSearchResults(&res)
+}
+
+// Untangle the XMLRPC mess, yay.
+func parseSearchResults(resp *interface{}) ([]Result, error) {
+	res := (*resp).(xmlrpc.Struct)
+	if res["status"] != SuccessStatus {
+		return nil, fmt.Errorf("Search error: %v", res["status"])
+	}
+
+	count := len(res["data"].(xmlrpc.Array))
+	results := make([]Result, count)
+
+	// FIXME debug mattn/go-xmlrpc, then
+	// FIXME build a slice of Result
+	//for _, raw := range res["data"].(xmlrpc.Struct) { }
+
+	return results, nil
+}
+
+// Login to the API. Return a token
+func Login(user string, pass string, lang string) (string, error) {
+	res, err := xmlrpc.Call(Server, "LogIn", user, pass, lang, UserAgent)
+	if err != nil {
+		return "", err
+	}
+	data := res.(xmlrpc.Struct)
+	if data["status"] != SuccessStatus {
+		return "", fmt.Errorf("login error: %s", data["status"])
+	}
+	return data["token"].(string), nil
+}
+
+// Logout
+func Logout(tok string) (err error) {
+	_, err = xmlrpc.Call(Server, "LogOut", tok)
+	return
+}
+
+// Read a chunk of a file at `offset` so as to fill `buf`.
 func readChunk(file *os.File, offset int64, buf []byte) (err error) {
 	n, err := file.ReadAt(buf, offset)
 	if err != nil {
