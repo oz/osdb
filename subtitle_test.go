1564ff0f4e62bf999e9f1bef4928300c90b808ff
Arnaud Berthomier <oz@cyprio.net>
2014-06-22 13:47:20 -0500

    More tests for Subtitle type

--- subtitle_test.go
+++ subtitle_test.go
@@ -1,6 +1,10 @@
 package osdb
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func TestBestWithNoResults(t *testing.T) {
 	subs := Subtitles{}
@@ -21,5 +25,52 @@ func TestBestWithResults(t *testing.T) {
 	}
 	if res.MovieName != "Go" {
 		t.Fatalf("Expected Go, got:", res)
+	}
+}
+
+func TestNewSubtitleWithFile_NoFile(t *testing.T) {
+	s, err := NewSubtitleWithFile("./no_movie_file", "./no_sub_file")
+	if err == nil {
+		t.Fatalf("Expected error, got subtitle:", s)
+	}
+}
+
+func TestNewSubtitleWithFile(t *testing.T) {
+	data := make([]byte, ChunkSize*2)
+
+	// Generate dummy movie file
+	copy(data, []byte("raw movie bytes go here"))
+	movieHash := "8a4f474701fbf13e"
+	err := ioutil.WriteFile("./test-movie.avi", data, 0644)
+	if err != nil {
+		t.Fatalf("Can't create test-movie.avi")
+	}
+	defer os.Remove("./test-movie.avi")
+
+	// Generate dummy subtitles file
+	copy(data, []byte("subtitle file data goes here"))
+	subHash := "95ad809f6ee45b779abf6d5337efeb44"
+	err = ioutil.WriteFile("./test-movie.srt", data, 0644)
+	if err != nil {
+		t.Fatalf("Can't create test-movie.srt")
+	}
+	defer os.Remove("./test-movie.srt")
+
+	s, err := NewSubtitleWithFile("./test-movie.avi", "./test-movie.srt")
+	if err != nil {
+		t.Fatalf("Expected Subtitle, got error:", err)
+	}
+
+	if s.SubFileName != "test-movie.srt" {
+		t.Fatalf("Expected SubFileName test-movie.srt, got %s", s.SubFileName)
+	}
+	if s.MovieFileName != "test-movie.avi" {
+		t.Fatalf("Expected MovieFileName test-movie.avi, got %s", s.MovieFileName)
+	}
+	if s.SubHash != subHash {
+		t.Fatalf("Expected subtitle Hash %s, got %s", subHash, s.SubHash)
+	}
+	if s.MovieHash != movieHash {
+		t.Fatalf("Expected movie Hash %s, got %s", movieHash, s.MovieHash)
 	}
 }
